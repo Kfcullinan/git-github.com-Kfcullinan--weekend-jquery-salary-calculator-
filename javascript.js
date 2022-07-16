ba4246881c7ec303d69892f83f14c40827d3a4a6
Keith Cullinan <kfcullinan@gmail.com>
2022-07-15 23:07:51 -0500

    added ready now function and started adding employee function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
 console.log("helloWorld");
+
+function readyNow() {
+    console.log('ready now')
+    $('addEmployeeButton').on('click', addEmployeeData);
+
+}
+
+function addEmployeeData() {
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let id = $('#idInput').val();
+    let title = $('#titleInput').val();
+    let yearlySalary = $('#annualSalaryInput').val();
+    $('#employees').append(`
+    <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${id}</td>
+                <td>${title}</td>
+                <td>${yearlySalary}</td>
+                <td><button id="deleteEmployee">Delete</button></td>
+                
+                
+            </tr>
+    `)
+
+}
